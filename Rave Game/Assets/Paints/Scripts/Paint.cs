54f7f72b442c184f31d759325caae918b9c65f0d
Ylfgard <sashamos1510@gmail.com>
2021-12-12 18:41:22 +0300

    Добавил краску

---
+++ Rave Game/Assets/Paints/Scripts/Paint.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Paint", menuName = "Rave Game/Paint", order = 0)]
+
+public class Paint : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
